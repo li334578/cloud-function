809d2856543ec35b19598239882f24728bdad6ee
liwenbo <liwenbo4922@163.com>
2021-03-01 14:09:17 +0800

    Initial commit

---
+++ src/main/java/com/example/cloudfunction/CloudFunctionApplication.java
@@ -0,0 +1,16 @@
+package com.example.cloudfunction;
+
+import cn.hutool.cron.CronUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFunctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFunctionApplication.class, args);
+        CronUtil.setMatchSecond(true);
+        CronUtil.start(true);
+    }
+
+}
